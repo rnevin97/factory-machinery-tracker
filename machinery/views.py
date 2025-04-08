4c18e34450db8fd1655212dafce15c87a4ae5d49
ragavantirangurumoorthy2501 <162064934+ragavantirangurumoorthy2501@users.noreply.github.com>
2025-04-08 22:29:36 +0100

    Update views.py

--- machinery/views.py
+++ machinery/views.py
@@ -1,66 +1,45 @@
-from django.http import HttpResponse , HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
-from django.utils import timezone
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
 from .models import Machine
 from .forms import MachineForm
 
-
 def index(request):
-    context = {}
-    return render(request, "machinery/index.html",context)
+    return render(request, "machinery/index.html")
 
 def aboutUs(request):
-    context = {}
-    return render(request, "machinery/about-us.html",context)
+    return render(request, "machinery/about-us.html")
 
 def faqs(request):
-    context = {}
-    return render(request, "machinery/faqs.html",context)
+    return render(request, "machinery/faqs.html")
 
 def login(request):
-    context = {}
-    return render(request, "machinery/login.html",context)
+    return render(request, "machinery/login.html")
 
 def register(request):
-    context = {}
-    return render(request, "machinery/register.html",context)
+    return render(request, "machinery/register.html")
 
 def services(request):
-    context = {}
-    return render(request, "machinery/services.html",context)  
+    return render(request, "machinery/services.html")
 
 def dashboard(request):
-    context = {}
-    return render(request, "machinery/dashboard.html",context)  
-
+    return render(request, "machinery/dashboard.html")
 
 def machines(request):
-    data = Machine.objects.all();
-    context = {
-        'data' : data
-    }
-    return render(request, "machinery/machines.html",context)
-
-
+    data = Machine.objects.all()
+    return render(request, "machinery/machines.html", {'data': data})
 
 def technicians(request):
-    context = {
-    }
-    return render(request, "machinery/technicians.html",context)
-
-    
+    return render(request, "machinery/technicians.html")
 
 def addMachine(request):
-    form =  MachineForm
+    form = MachineForm
     if request.method == 'POST':
         saveForm = MachineForm(request.POST)
         if saveForm.is_valid():
-            saveForm.save();
+            saveForm.save()
             return HttpResponseRedirect("/machines")
+    return render(request, "machinery/addMachine.html", {'form': form})
 
-    return render(request, "machinery/addMachine.html",{'form' : form})
-
-def deleteMachine(request , id) :
+def deleteMachine(request, id):
     Machine.objects.filter(id=id).delete()
     return HttpResponseRedirect("/machines")
