c0138e001f6379da91205d47a18390ae286258cd
ragavantirangurumoorthy2501 <162064934+ragavantirangurumoorthy2501@users.noreply.github.com>
2025-04-08 22:23:02 +0100

    Update urls.py

--- machinery/urls.py
+++ machinery/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import login_user, logout_user, RegistrationAPIView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
+    # Web App Views
     path("", views.index, name="index"),
     path("home", views.index, name="index"),
     path("about-us", views.aboutUs, name="aboutUs"),
@@ -14,4 +17,10 @@ urlpatterns = [
     path("technicians", views.technicians, name="technicians"),
     path("addMachine", views.addMachine, name="addMachine"),
     path("deleteMachine/<int:id>", views.deleteMachine, name="deleteMachine"),
+
+    # API Routes
+    path("api/login/", login_user, name="api_login"),
+    path("api/logout/", logout_user, name="api_logout"),
+    path("api/register/", RegistrationAPIView.as_view(), name="api_register"),
+    path("api/token-auth/", obtain_auth_token),
 ]
