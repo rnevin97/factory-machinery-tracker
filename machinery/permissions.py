cbe9fd4b70f22d4acc30028d5bca6d0d0ced8493
ragavantirangurumoorthy2501 <162064934+ragavantirangurumoorthy2501@users.noreply.github.com>
2025-04-08 22:09:54 +0100

    Update permissions.py

--- machinery/permissions.py
+++ machinery/permissions.py
@@ -1,1 +1,22 @@
+from rest_framework.permissions import BasePermission
+from .models import UserProfile
 
+class IsManagerOrTechnician(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                return profile.role in ['manager', 'technician']
+            except UserProfile.DoesNotExist:
+                return False
+        return False
+
+class IsTechnicianOrRepair(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                return profile.role in ['technician', 'repair_person']
+            except UserProfile.DoesNotExist:
+                return False
+        return False
