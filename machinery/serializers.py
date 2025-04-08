f21efe46e3239eebb57ff3358e639f207cf1d5aa
ragavantirangurumoorthy2501 <162064934+ragavantirangurumoorthy2501@users.noreply.github.com>
2025-04-08 22:08:58 +0100

    Update serializers.py

--- machinery/serializers.py
+++ machinery/serializers.py
@@ -1,1 +1,37 @@
+from rest_framework import serializers
+from .models import Machine, RepairRequest, MaintenanceRecord, Warning, UserProfile
+from django.contrib.auth.models import User
+
+
+class MachineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Machine
+        fields = '__all__'
+
+class RepairRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RepairRequest
+        fields = '__all__'
+
+class MaintenanceRecordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MaintenanceRecord
+        fields = '__all__'
+
+class WarningSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Warning
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = UserProfile
+        fields = ['user', 'role']
 
