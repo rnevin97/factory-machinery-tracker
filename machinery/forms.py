44b4bd3e462618601bdc3e50d518fe411efeb04d
Zoha5 <zoekayani@gmail.com>
2025-04-10 21:58:21 +0100

    repair requests
    1. Added new pages to show repair requests
    2. Create new request form implemented
    3. Update request model to select request status
    4. Modify and delete repair request feature is implemented

--- machinery/forms.py
+++ machinery/forms.py
@@ -1,7 +1,21 @@
 from django import forms
-from .models import Machine
+from .models import Machine, RepairRequest
 
 class MachineForm(forms.ModelForm):
     class Meta:
         model = Machine
         fields = ('id','name','serial_number','importance','status')
+
+
+class RepairRequestForm(forms.ModelForm):
+    class Meta:
+        model = RepairRequest
+        fields = ['machine', 'issue_description', 'status']  # only fields user should input
+        widgets = {
+            'issue_description': forms.Textarea(attrs={'rows': 4}),
+             'status': forms.Select(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(RepairRequestForm, self).__init__(*args, **kwargs)
+        self.fields['machine'].queryset = Machine.objects.all()
--- machinery/urls.py
+++ machinery/urls.py
@@ -13,11 +13,16 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("services", views.services, name="services"),
     path("dashboard", views.dashboard, name="dashboard"),
-    path("machines", views.machines, name="machines"),
     path("technicians", views.technicians, name="technicians"),
+    path("machines", views.machines, name="machines"),
     path("addMachine", views.addMachine, name="addMachine"),
     path("deleteMachine/<int:id>", views.deleteMachine, name="deleteMachine"),
     path("modifyMachine/<int:id>/", views.modifyMachine, name="modifyMachine"),
+    path("repairRequests", views.repairRequests, name="repairRequests"),
+    path("addRepairRequest", views.addRepairRequest, name="addRepairRequest"),
+    path("deleteRepairRequest/<int:id>", views.deleteRepairRequest, name="deleteRepairRequest"),
+    path("modifyRepairRequest/<int:id>/", views.modifyRepairRequest, name="modifyRepairRequest"),
+
 
     # API Routes (from api_views)
     path("api/login/", api_views.login_user, name="api_login"),
